76155b011eb7f8ff22079589f7111660aab07051
Marc Mills <mdmills1990@gmail.com>
2018-10-25 17:04:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,18 @@
 class Student
+attr_accessor :name, :grade
+attr_reader :id
 
+def self.create_table
+end
+
+def self.drop_table
+end
+
+def save
+end
+
+def self.create
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
