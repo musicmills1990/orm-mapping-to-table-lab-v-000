8647497a28b226c5ace52b34052296df0a1d6e98
Marc Mills <mdmills1990@gmail.com>
2018-10-25 17:08:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,14 @@ def initialize(name, grade, id = nil)
 end
 
 def self.create_table
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER)
+  SQL
+
+  DB[:conn].execute(sql)
 end
 
 def self.drop_table
