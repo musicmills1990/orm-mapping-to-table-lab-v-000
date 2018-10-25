c9f7417741822ba9ecc993432b889a565c0f4211
Marc Mills <mdmills1990@gmail.com>
2018-10-25 17:05:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,12 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
+def initialize(name, grade, id = nil)
+  @name = name
+  @grade = grade
+  @id = id
+end
+
 def self.create_table
 end
 
